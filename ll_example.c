fb9d0de794086464ba1ac95c4da992a4f599517c
plainlee <plain.lee@student.mahidol.edu>
2026-02-16 09:28:40 +0000

    Fixed 1st attempt

--- ll_example.c
+++ ll_example.c
@@ -58,9 +58,91 @@
 // } // end main
 
 
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// #include "ll.h"
+
+// int main( void )
+// {
+//    LLPtr startPtr = NULL;
+//    unsigned int choice;
+//    int id;
+//    char name[50];
+//    char buffer[100];
+
+//    instructions();
+
+//    while ( 1 ) {
+//       printf( "%s", "? " );
+
+     
+//       if ( fgets(buffer, sizeof(buffer), stdin) == NULL ) break;
+
+      
+//       if ( sscanf(buffer, "%u", &choice) != 1 ) {
+//          puts( "Invalid choice." );
+//          instructions();
+//          continue; 
+//       }
+
+//       if ( choice == 3 ) break;
+
+//       switch ( choice ) {
+//          case 1:
+//             printf( "%s", "Enter id and name: " );
+//             while ( 1 ) {
+//                 if ( fgets(buffer, sizeof(buffer), stdin) != NULL ) {
+//                     if ( sscanf(buffer, "%d %49s", &id, name) == 2 ) {
+//                         break;
+//                     }
+//                 }
+//                 printf( "Invalid input. Try again: " );
+//             }
+
+//             insert( &startPtr, id, name );
+//             printList( startPtr );
+//             break;
+
+//          case 2:
+//             if ( !isEmpty( startPtr ) ) {
+//                printf( "%s", "Enter number to be deleted: " );
+//                while ( 1 ) {
+//                     if ( fgets(buffer, sizeof(buffer), stdin) != NULL ) {
+//                         if ( sscanf(buffer, "%d", &id) == 1 ) break;
+//                     }
+//                     printf( "Invalid input. Enter number to be deleted again: " );
+//                }
+
+//                if ( deletes( &startPtr, id ) ) {
+//                   printf( "id %d deleted.\n", id );
+//                   printList( startPtr );
+//                }
+//                else {
+//                   printf( "id %d not found.\n\n", id );
+//                }
+//             }
+//             else {
+//                puts( "List is empty.\n" );
+//             }
+//             break;
+
+//          default:
+//             puts( "Invalid choice.\n" );
+//             instructions();
+//             break;
+//       }
+//    }
+
+//    puts( "Clear all nodes" );
+//    clearList( &startPtr );
+//    puts( "End of run." );
+   
+//    return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "ll.h"
 
 int main( void )
@@ -69,19 +151,19 @@ int main( void )
    unsigned int choice;
    int id;
    char name[50];
-   char buffer[100];
+   int status; 
 
    instructions();
 
    while ( 1 ) {
       printf( "%s", "? " );
-
-     
-      if ( fgets(buffer, sizeof(buffer), stdin) == NULL ) break;
-
+      status = scanf( "%u", &choice );
+      if ( status == EOF ) break; 
       
-      if ( sscanf(buffer, "%u", &choice) != 1 ) {
+      if ( status != 1 ) {
          puts( "Invalid choice." );
+         int c;
+         while ((c = getchar()) != '\n' && c != EOF); 
          instructions();
          continue; 
       }
@@ -90,14 +172,18 @@ int main( void )
 
       switch ( choice ) {
          case 1:
-            printf( "%s", "Enter id and name: " );
+            printf( "%s", "Enter id and name: " ); 
             while ( 1 ) {
-                if ( fgets(buffer, sizeof(buffer), stdin) != NULL ) {
-                    if ( sscanf(buffer, "%d %49s", &id, name) == 2 ) {
-                        break;
-                    }
+                status = scanf( "%d", &id );
+                if ( status == EOF ) goto end_of_run; 
+                if ( status == 1 ) {
+                    scanf( "%49s", name ); 
+                    break;
                 }
-                printf( "Invalid input. Try again: " );
+                
+                puts( "Invalid input. Enter id and name again: " );
+                int c;
+                while ((c = getchar()) != '\n' && c != EOF);
             }
 
             insert( &startPtr, id, name );
@@ -106,12 +192,15 @@ int main( void )
 
          case 2:
             if ( !isEmpty( startPtr ) ) {
-               printf( "%s", "Enter number to be deleted: " );
+               printf( "%s", "Enter id to be deleted: " );
                while ( 1 ) {
-                    if ( fgets(buffer, sizeof(buffer), stdin) != NULL ) {
-                        if ( sscanf(buffer, "%d", &id) == 1 ) break;
-                    }
-                    printf( "Invalid input. Enter number to be deleted again: " );
+                    status = scanf( "%d", &id );
+                    if ( status == EOF ) goto end_of_run;
+                    if ( status == 1 ) break;
+                    
+                    puts( "Invalid input. Enter id again: " );
+                    int c;
+                    while ((c = getchar()) != '\n' && c != EOF);
                }
 
                if ( deletes( &startPtr, id ) ) {
@@ -134,6 +223,7 @@ int main( void )
       }
    }
 
+end_of_run:
    puts( "Clear all nodes" );
    clearList( &startPtr );
    puts( "End of run." );
